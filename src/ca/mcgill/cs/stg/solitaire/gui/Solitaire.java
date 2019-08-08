bc23130a17c53d0c2a507246c21b3990d2ab83c7
prmr <prmr@gmx.com>
2019-08-08 14:54:44 -0400

    Update version string

--- src/ca/mcgill/cs/stg/solitaire/gui/Solitaire.java
+++ src/ca/mcgill/cs/stg/solitaire/gui/Solitaire.java
@@ -45,7 +45,7 @@ public class Solitaire extends Application
 	private static final int HEIGHT = 500;
 	private static final int MARGIN_OUTER = 10;
 	private static final String TITLE = "Solitaire";
-	private static final String VERSION = "0.3";
+	private static final String VERSION = "1.0";
 
     private DeckView aDeckView = new DeckView();
     private DiscardPileView aDiscardPileView = new DiscardPileView();
