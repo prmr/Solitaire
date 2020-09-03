6eaee05ce1f71664a20b0c891906645fdd874377
prmr <prmr@gmx.com>
2020-09-03 12:23:04 -0400

    #24 Modularize

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module solitaire
+{
+	exports ca.mcgill.cs.stg.solitaire.gui;
+
+	requires javafx.base;
+	requires javafx.controls;
+	requires transitive javafx.graphics;
+	requires static junit;
+}
